6a6c140552117ac96ab7ae3030bd32e30cb6362c
Aditi Dubey <aditidubey073@gmail.com>
2026-09-01 21:59:50 +0530

    else if

---
+++ javaScript/3rd/index.js
@@ -0,0 +1,10 @@
+let age =6;
+age+=14;
+
+if(age>=18){
+    console.log("You are an adult.");
+}else if(age==0){
+    console.log("You are a baby.");
+}else{
+    console.log("You are a child.");
+}   
