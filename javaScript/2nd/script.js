4eaeb90ab5e631b1d0f9afa1f723709a030bc7bf
Aditi Dubey <aditidubey073@gmail.com>
2026-09-01 21:57:12 +0530

    lec 2 primitive datatypes and objects

---
+++ javaScript/2nd/script.js
@@ -0,0 +1,21 @@
+var a=8;
+{
+    a=10;
+    console.log(a);
+}
+console.log(a);
+
+let b=10;
+{ b=20;
+    console.log(b);
+}
+console.log(b);
+
+const c=30;
+
+let o={
+    name:"sachin",
+    age:22,}
+
+    o.name="sachin kumar";
+    console.log(o);
