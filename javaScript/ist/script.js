aa2a70c56c15020984bb53f353867c1ca127efbb
Aditi Dubey <aditidubey073@gmail.com>
2026-09-01 20:06:51 +0530

    1st

---
+++ javaScript/ist/script.js
@@ -0,0 +1,13 @@
+console.log("hello world");
+alert("welcome");
+var a=prompt("enter a number");
+console.log("you enterred",a);
+var isTrue=confirm("do you want to blast computer");
+if(isTrue(a)){
+    console.log("yes");
+}else{
+    console.log("no");
+}
+
+document.title="javaScript class 1";
+document.body.style.backgroundColor="red";
